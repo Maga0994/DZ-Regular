f833f8b8fd78bbabfc2e6a19479b2b97e4433613
Gadzhaev Magomet <gadzhaev.magomet@gmail.com>
2024-05-07 00:27:04 +0300

    Fix code

--- main.py
+++ main.py
@@ -9,6 +9,28 @@ with open('C:\\Users\\user\\Desktop\\DZ Regular\\phonebook_raw.csv', encoding="u
     contacts_list = list(rows)
 pprint(contacts_list)
 
+
+def format_name(full_name):
+    # Разбиваем полное имя на части по пробелам
+    name_parts = full_name.split()
+    # Извлекаем фамилию
+    lastname = name_parts[0]
+    # Извлекаем имя
+    firstname = name_parts[1]
+    # Если указано отчество, добавляем его
+    surname = name_parts[2] if len(name_parts) > 2 else 'Владимировна'
+
+    return lastname, firstname, surname
+
+
+def unique_phone_and_email(contact, added_phones, added_emails):
+    # Проверка уникальности телефона и email
+    phone = contact[5]
+    email = contact[6]
+
+    return phone not in added_phones and email not in added_emails
+
+
 def format_phone(phone):
     phone_pattern = r'\+?(\d{1,2})?[\s\(\-\)]*?(\d{3})[\s\(\-\)]*?(\d{3})[\s\(\-\)]*?(\d{2})[\s\(\-\)]*?(\d{2})[\s\(\-\)]*?(доб\.\s*\d{4})?'
 
@@ -25,27 +47,41 @@ def format_phone(phone):
 
 
 def format_contacts(raw_contacts):
-    formatted_contacts = []
-    added_names = set()  # Для хранения добавленных имен
     added_phones = set()  # Для хранения добавленных номеров телефонов
+    contacts_dict = {}  # Словарь для хранения контактов по ключу (фамилия, имя)
 
     for contact in raw_contacts[1:]:
-        lastname, firstname, surname, organization, position, phone, email = contact
-
-        name_parts = firstname.split()
-        if len(name_parts) > 1:
-            firstname = name_parts[0]
-            surname = " ".join(name_parts[1:])
+        lastname, firstname, surname = format_name(" ".join(contact[:2]))
+        organization = contact[3]
+        position = contact[4]
+        phone = contact[5]
+        email = contact[6]
 
         formatted_phone = format_phone(phone)
-
-        # Проверка на уникальность контактов по Фамилии и Имени и номеру телефона
-        if (lastname, firstname) not in added_names and formatted_phone not in added_phones:
-            formatted_contacts.append([lastname, firstname, surname, organization, position, formatted_phone, email])
-            added_names.add((lastname, firstname))
+        # Проверка на уникальность по номеру телефона
+        if formatted_phone not in added_phones:
             added_phones.add(formatted_phone)
+        key = (lastname, firstname)
+
+        if key in contacts_dict:
+            # Объединяем контакты с одинаковой фамилией и именем
+            existing_contact = contacts_dict[key]
+            if not existing_contact[3]:
+                existing_contact[3] = organization
+            if not existing_contact[4]:
+                existing_contact[4] = position
+            if not existing_contact[5]:
+                existing_contact[5] = phone
+            if not existing_contact[6]:
+                existing_contact[6] = email
+        else:
+            contacts_dict[key] = [lastname, firstname, surname, organization, position, phone, email]
+
+    # Формируем список отформатированных контактов
+    formatted_contacts = [contact for contact in contacts_dict.values()]
 
     return formatted_contacts
+
 
 # Получаем отформатированные данные
 formatted_contacts = format_contacts(contacts_list)
@@ -64,6 +100,4 @@ with open(output_file, mode='w', encoding="utf-8", newline='') as f:
     for contact in formatted_contacts:
         writer.writerow(contact)
 
-
 print(f"Данные успешно записаны в файл: {output_file}")
-
